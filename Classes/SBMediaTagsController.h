da20fa65c3b15862d805b157e99d013d5dd0626e
Damiano Galassi <damiog@gmail.com>
2015-09-13 08:02:48 +0200

    Add a window to select the desired media characteristic tags

---
+++ Classes/SBMediaTagsController.h
@@ -0,0 +1,13 @@
+//
+//  SBMediaTagsController.h
+//  Subler
+//
+//  Created by Damiano Galassi on 12/09/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SBMediaTagsController : NSWindowController
+
+@end
---
+++ Classes/SBMediaTagsController.m
@@ -0,0 +1,23 @@
+//
+//  SBMediaTagsController.m
+//  Subler
+//
+//  Created by Damiano Galassi on 12/09/15.
+//
+//
+
+#import "SBMediaTagsController.h"
+
+@interface SBMediaTagsController ()
+
+@end
+
+@implementation SBMediaTagsController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
