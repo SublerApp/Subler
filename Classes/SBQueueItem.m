a872eda5d3ddb675769bcd302573c70bdda0d64f
Brian Gerfort <bitbucket@2ndnaturesoftware.com>
2017-05-05 15:41:25 +0200

    * Support for DTS to AC3 + AAC conversion.
    If you prefer to recompile FFmpeg from scratch, make sure you include the --enable-encoder=ac3 and --disable-iconv parameters. My config:
    
    ./configure --disable-programs --disable-filters --disable-everything --disable-avdevice --disable-swscale --enable-avresample --enable-decoder=dvdsub --enable-decoder=pgssub --enable-decoder=aac --enable-decoder=mp3 --enable-decoder=mp2 --enable-decoder=mp1 --enable-decoder=ac3 --enable-decoder=eac3 --enable-decoder=truehd --enable-decoder=alac --enable-decoder=opus --enable-decoder=vorbis --enable-decoder=flac --enable-decoder=mlp --enable-decoder=dca --enable-decoder=pcm --enable-filter=aresample --enable-parser=opus --enable-muxer=opus --prefix=/Users/damians/Desktop/iffmpeg --extra-cxxflags='-mmacosx-version-min=10.9' --extra-cflags='-mmacosx-version-min=10.9' --extra-ldflags='-mmacosx-version-min=10.9' --enable-decoder=pcm_f32be --enable-decoder=pcm_f32le --enable-decoder=pcm_f64be --enable-decoder=pcm_f64le --enable-decoder=pcm_s16be --enable-decoder=pcm_s16le --enable-decoder=pcm_s16le_planar --enable-decoder=pcm_s24be --enable-decoder=pcm_daud --enable-decoder=pcm_s24le --enable-decoder=pcm_s32be --enable-decoder=pcm_s32le --enable-decoder=pcm_s8 --enable-decoder=pcm_u8 --enable-decoder=pcm_u16be --enable-decoder=pcm_u16le --enable-decoder=pcm_u24be --enable-decoder=pcm_u24le --enable-encoder=ac3 --disable-iconv

--- Classes/SBQueueItem.m
+++ Classes/SBQueueItem.m
@@ -210,17 +210,39 @@
             // DTS -> convert only if specified in the prefs.
             else if (track.format == kMP42AudioCodecType_DTS) {
                 if ([[[NSUserDefaults standardUserDefaults] valueForKey:@"SBAudioConvertDts"] boolValue]) {
-                    if ([[[NSUserDefaults standardUserDefaults] valueForKey:@"SBAudioKeepDts"] boolValue]) {
-                        MP42AudioTrack *copy = [track copy];
-                        MP42ConversionSettings *settings = [MP42AudioConversionSettings audioConversionWithBitRate:bitRate
-                                                                                                           mixDown:SBDolbyPlIIMixdown
-                                                                                                               drc:drc];
-                        copy.conversionSettings = settings;
-
-                        [self.mp4File addTrack:copy];
-                    }
-                    else {
-                        conversionNeeded = YES;
+                    switch ([[[NSUserDefaults standardUserDefaults] valueForKey:@"SBAudioDtsOptions"] integerValue]) {
+                        case 1: { // Convert to AC-3
+                            MP42AudioTrack *copy = [track copy];
+                            MP42ConversionSettings *settings = [MP42AudioConversionSettings audioConversionWithBitRate:bitRate
+                                                                                                               mixDown:SBDolbyPlIIMixdown
+                                                                                                                   drc:drc];
+                            copy.conversionSettings = settings;
+                            
+                            ((MP42AudioTrack *)track).fallbackTrack = copy;
+                            track.enabled = NO;
+                            track.conversionSettings = [[MP42AudioConversionSettings alloc] initWitFormat:kMP42AudioCodecType_AC3
+                                                                                                  bitRate:640
+                                                                                                  mixDown:SBNoneMixdown
+                                                                                                      drc:drc];
+                            [self.mp4File addTrack:copy];
+                            break;
+                        }
+                        case 2: { // Keep DTS
+                            MP42AudioTrack *copy = [track copy];
+                            MP42ConversionSettings *settings = [MP42AudioConversionSettings audioConversionWithBitRate:bitRate
+                                                                                                               mixDown:SBDolbyPlIIMixdown
+                                                                                                                   drc:drc];
+                            copy.conversionSettings = settings;
+                            
+                            ((MP42AudioTrack *)track).fallbackTrack = copy;
+                            track.enabled = NO;
+                            
+                            [self.mp4File addTrack:copy];
+                            break;
+                        }
+                        default:
+                            conversionNeeded = YES;
+                            break;
                     }
                 }
             }
